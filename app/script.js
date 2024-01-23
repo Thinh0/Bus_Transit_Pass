3a8ef9e832ab2b279f70be3eeab06c5b572990e4
Duc Thinh Nguyen <dnguyen4200@conestogac.on.ca>
2024-01-22 23:40:58 -0500

    add generate pass button

---
+++ app/script.js
@@ -0,0 +1,7 @@
+
+function generateToRegenerate() {
+    document.getElementById("button").innerHTML = "Regenerate Pass";
+    var bussPassNumber = document.getElementById("passNumber");
+    bussPassNumber.textContent = "Your Bus Pass number is: ";
+    document.getElementById("activate").innerHTML = "Activate Pass";
+}
--- react/src/Login.js
+++ react/src/Login.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=03ef4624c1054b4e866fc93a8f337376' +
   '&response_type=code&redirect_uri=http://localhost:3000' +
-  '&scope=streaming user-read user-modify-playback-state user-read-email user-read-playback-state user-read-email user-read-private user-read-library user-library-modify user-library-read user-library-modify user-read-playback-state user-modify-playback-state';
+  '&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 export default function Login() {
     return (
