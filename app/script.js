388bb3ac4fc73881902491eb6dab6d5c96a9b45d
Duc Thinh Nguyen <dnguyen4200@conestogac.on.ca>
2024-01-25 19:28:41 -0500

    added generateBusPass

--- app/script.js
+++ app/script.js
@@ -1,7 +1,14 @@
 
 function generateToRegenerate() {
+
     document.getElementById("button").innerHTML = "Regenerate Pass";
-    var bussPassNumber = document.getElementById("passNumber");
-    bussPassNumber.textContent = "Your Bus Pass number is: ";
+    document.getElementById("button").style.backgroundColor = 'transparent';
+    document.getElementById("button").style.color = '#8F3985';
+    var busPassNumber = document.getElementById("passNumber");
+    busPassNumber.textContent = "Your Bus Pass number is: ";
+    const randomNumbers = "Your Bus Pass Number is: BTP" + Math.floor(1000000 + Math.random() * 9000000);
+    document.getElementById("passNumber").innerHTML = randomNumbers;
     document.getElementById("activate").innerHTML = "Activate Pass";
+    
+    return busPass;
 }
